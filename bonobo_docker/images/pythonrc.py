a59124c3bbc15a06dd8ac2b85a48c09be8d372f7
Romain Dorgueil <romain@dorgueil.net>
2017-06-05 10:26:30 +0200

    Preload bonobo module in containers REPL.

--- bonobo_docker/images/pythonrc.py
+++ bonobo_docker/images/pythonrc.py
@@ -9,14 +9,18 @@ def _enable_syntax_completion():
         import rlcompleter
         readline.parse_and_bind("tab: complete")
 
+
 def _enable_shell_colors():
     import sys
     from colorama import Fore, Style
-    sys.ps1 = Fore.LIGHTWHITE_EX+'🐵 >'+Fore.RESET+' '
-    sys.ps2 = Fore.LIGHTBLACK_EX+'...'+Fore.RESET+' '
+    sys.ps1 = Fore.LIGHTWHITE_EX + '🐵 >' + Fore.RESET + ' '
+    sys.ps2 = Fore.BLACK + '..' + Fore.LIGHTBLACK_EX + '.' + Fore.RESET + ' '
 
 
 _enable_syntax_completion()
 _enable_shell_colors()
 del _enable_syntax_completion
 del _enable_shell_colors
+
+# Preload bonobo
+bonobo = __import__('bonobo')
